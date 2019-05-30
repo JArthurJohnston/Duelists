a9e96f5f91b34041451b167883a44b6fe45467ff
JArthurJohnston <joe.a.johnston@gmail.com>
2019-05-30 07:39:15 -0400

    added basic attack logic to the state machine and combatant controller

--- Assets/Scripts/CombatAI/States/AttackingState.cs
+++ Assets/Scripts/CombatAI/States/AttackingState.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class AttackingState : CombatantState
 {
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        
+        combatant.AttackTarget = FindAttackTarget();
+    }
+
     // Start is called before the first frame update
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -11,5 +19,25 @@ public class AttackingState : CombatantState
         //wait for a random delay
         //attempt attack
         //set right of way boolean
+
+        /*
+        if(AttacckHasFinished) //distance from hand to shoulder is >= arm length
+            combatant.MoveHandTo(READY_POSITION);
+        if(AttackHasLanded){
+            combatant.Sore++;
+            animator.SetSomethingToGoBackToReadyPositions()
+        }
+         */
+    }
+
+    void Wait(float seconds){
+        float delayedTime = 0;
+        while(delayedTime < seconds){
+            delayedTime += Time.deltaTime;
+        }
+    }
+    GameObject FindAttackTarget(){
+        int targetIndex = Random.Range(0, 3);
+        return combatant.Oponent.Targets[targetIndex];
     }
 }
--- Assets/Scripts/CombatantController.cs
+++ Assets/Scripts/CombatantController.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class CombatantController : MonoBehaviour
 {
+    private GameObject _attackTarget;
+    public GameObject WeaponShoulder;
+    public GameObject WeaponHand;
+    public float ArmLength = 2;
+    public GameObject AttackTarget {
+        get{return _attackTarget;} 
+        set {
+            if(_attackTarget == null){
+                StartCoroutine(StartAttackingTarget(value));
+            }
+        }
+    }
+    public float attackSpeed = 15;
+    public bool IsDebug = false;
     private Vector3 LookTarget;
     public GameObject[] Targets;
     public CombatantController Oponent;
@@ -15,6 +29,28 @@ public class CombatantController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(_attackTarget != null){
+            WeaponHand.transform.LookAt(_attackTarget.transform);
+            float step = attackSpeed * Time.deltaTime;
+            if(Vector3.Distance(WeaponHand.transform.position, WeaponShoulder.transform.position) < ArmLength){
+                WeaponHand.transform.position = 
+                    Vector3.MoveTowards(WeaponHand.transform.position, _attackTarget.transform.position, step);
+            }
+        }
+    }
+
+    IEnumerator StartAttackingTarget(GameObject target){
+        float delay = Random.Range(0.5f, 2f);
+        yield return new WaitForSeconds(delay);
+        _attackTarget = target;
+    }
+
+    void HandleDebugInputs(){
+        if(IsDebug){
+            if(Input.GetKeyDown("1")){
+
+            }
+        }
     }
 
     public Vector3 Position(){
